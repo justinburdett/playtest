eba97454ae78681095fb17364fa2f1f02f2834ad
Aaron (Home) <armahillo@gmail.com>
2013-11-28 00:51:25 -0500

    Refactors HTML rendering slightly. Fleshes out RSpec coverage further. HTML Rendering now correctly displays paginated 3x3 grids of cards.

--- lib/template.rb
+++ lib/template.rb
@@ -8,24 +8,39 @@ class Template
     @html = html
     @css = css
     @document = ""
-    @cards_added = 0
+    @cards = {}
+    @pages = []
   end
 
-  def <<(name, details)
-    additional_classes = details.delete('css')
-    quantity = details.delete('quantity') || 1
-    card_text = "<dl>"
-    details.each do |k,v| card_text += "<dt class=\"#{k}\">#{k}</dt><dd class=\"#{k}\">#{v}</dd>"; end
-    quantity.times { @document += "<div class=\"card #{additional_classes}\"><h2>#{name}</h2>#{card_text}</div>\r\n" }
-    @cards_added += quantity
-    puts "Added #{quantity} '#{name}' (#{@cards_added} total)"
+  def <<(details)
+    name = details["name"] ||= "No Name #{unique_cards}"
+    details["quantity"] ||= 1
+    if (@cards.has_key?(name.to_sym)) 
+      @cards[name.to_sym]["quantity"] += (details["quantity"] || 1)
+    else
+      @cards[name.to_sym] = details
+    end
+    puts "[#{@cards.keys.index(name.to_sym)}] Added #{details["quantity"]} '#{name}' (#{total_cards} total)"
+  end
+  
+  def total_cards
+    @cards.values.find_all {|property| property == "quantity"}.reduce(:+)
+  end
+  
+  def unique_cards
+    @cards.keys.count
   end
  
   def to_html(destination = "export/exported_#{@html}")
+    render_cards
     css_data = File.read(@css)
     export = "<html>
-       <head><style type=\"text/css\" media=\"all\">#{css_data}</style></head>
-       <body class=\"content\">#{@document}
+       <head><style type=\"text/css\" media=\"all\">#{css_data}</style>
+             <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"></head>
+       <body class=\"content\">
+         <table class=\"poker\">
+         #{@document}
+         </table>
        </body>
        </html>"
     File.open(destination, 'w') { |file| file.write(export) }
@@ -33,8 +48,9 @@ class Template
   end
  
   def to_pdf(destination = "export/#{@html}.pdf")
+    render_cards
     # Generate the PDF using the HTML we've generated
-    kit = PDFKit.new("<html><body class=\"content\">#{@document}</body></html>", :page_size => 'Letter', :print_media_type => true)
+    kit = PDFKit.new("<html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"></head><body class=\"content\">#{@document}</body></html>", :page_size => 'Letter', :print_media_type => true)
 
     # Add the external cards.css stylesheet to our PDF
     kit.stylesheets << @css
@@ -42,5 +58,34 @@ class Template
     # Save the PDF to our machine
     kit.to_file(destination)
     puts "Saved #{@cards_added} cards to #{destination} (#{File.size?(destination)})"
+  end
+  
+private
+  def render_cards(card_type = "poker")
+    # Reset the document property so that we can re-render if necessary
+    @document = ""
+    # This loop iterates through all the cards that have been added
+    cards_rendered = 0
+    @cards.each do |name, card_details|
+      additional_classes = card_details.delete("css")
+      quantity = card_details.delete("quantity")
+      card_text = "<h2>#{name.to_s}</h2>"
+      card_text += "<dl>"
+      card_details.each do |k,v| card_text += "<dt class=\"#{k}\">#{k}</dt><dd class=\"#{k}\">#{v}</dd>"; end
+      card_text += "</dl>"
+      quantity.times do |i|
+        # We need to create a new table every 9 cards
+        @document += "<table class=\"#{card_type}\">" if (cards_rendered % 9 == 0)
+        # We also need a new table row every 3 cards
+        @document += "<tr>" if (cards_rendered % 3 == 0)
+        # Insert the card details here
+        @document += "<td class=\"card #{additional_classes}\">#{card_text}</td>"
+        
+        # Increment cards rendered. This may trigger HTML closing tags
+        cards_rendered += 1
+        @document += "</tr>" if (cards_rendered % 3 == 0)
+        @document += "</table>" if (cards_rendered % 9 == 0)
+      end
+    end
   end
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,7 +1,45 @@
 require 'deck'
 
-describe "default" do
-  it "creates a deck object" do
-    expect(Deck.new.type).to eq(Deck)
+describe "Deck" do
+
+  let!(:card_source) { "spec/data/cards.yml" }
+  let!(:alternate_card_source) { "spec/data/alternate_cards.yml" }
+
+  describe "Constructor" do
+    it "instantiates with no arguments" do
+      expect(Deck.new).to be_instance_of(Deck)
+    end
+    it "can override the source file with an argument" do
+      d = Deck.new(card_source)
+      expect(d).to be_instance_of(Deck)
+      expect(d.cards).to_not be_empty
+    end
+  end
+
+  describe "Attributes" do
+    let!(:deck) { Deck.new }
+  
+    it "can access the cards read-only" do
+      expect(deck.cards.count).to_not be_nil
+      expect { deck.cards = 5 }.to raise_error(NoMethodError)
+    end
+  end
+
+  describe "Methods" do
+    let!(:deck) { Deck.new }
+  
+    it "can load a new YAML file" do
+      expect {
+        deck.load(alternate_card_source)
+      }.to change{deck.cards.count}
+    end
+  
+    context "when enumerating" do
+      it "prepares the details hash" do
+        details = deck.first
+        expect(details).to_not be_nil
+        expect(details["name"]).to_not be_nil
+      end
+    end
   end
 end
